64b3ff474ae1ea52234456382cb517de912fbc5f
server <xiangwei>
2023-03-11 16:51:42 +0800

    tmp

---
+++ config.py
@@ -0,0 +1,40 @@
+import argparse
+import tensorflow as tf
+
+
+def get_config():
+    parser = argparse.ArgumentParser("Reinforcement Learning experiments for multiagent environments")
+    # Environment
+    parser.add_argument("--scenario", type=str, default="formation_v1", help="定义要使用 MPE 中的哪个环境")
+    parser.add_argument("--algo_name", type=str, default="ma-ddpg", help="算法名称")
+    parser.add_argument("--device", type=str, default="cuda" if tf.test.is_gpu_available() else 'cpu', help="检测GPU")
+    parser.add_argument("--episodes_num", type=int, default=60000, help="训练的回合数")
+
+    parser.add_argument("--max-episode-len", type=int, default=240, help="每回合的步数")
+
+    parser.add_argument("--num-adversaries", type=int, default=0, help="number of adversaries")
+    parser.add_argument("--good-policy", type=str, default="maddpg", help="policy for good agents")
+    parser.add_argument("--adv-policy", type=str, default="maddpg", help="policy of adversaries")
+    # Core training parameters
+    parser.add_argument("--lr", type=float, default=1e-2, help="learning rate for Adam optimizer")
+    parser.add_argument("--gamma", type=float, default=0.95, help="discount factor")
+    parser.add_argument("--batch-size", type=int, default=1024, help="number of episodes to optimize at the same time")
+    parser.add_argument("--num-units", type=int, default=64, help="number of units in the mlp")
+    # Checkpointing
+    parser.add_argument("--exp-name", type=str, default='', help="name of the experiment")
+    parser.add_argument("--save-dir", type=str, default=parent_path,
+                        help="directory in which training state and model should be saved")
+    parser.add_argument("--save-rate", type=int, default=500,
+                        help="save model once every time this many episodes are completed")
+    parser.add_argument("--load-dir", type=str, default="",
+                        help="directory in which training state and model are loaded")
+    # Evaluation
+    parser.add_argument("--restore", action="store_true", default=False)
+    parser.add_argument("--display", action="store_true", default=True)
+    parser.add_argument("--benchmark", action="store_true", default=False)
+    parser.add_argument("--benchmark-iters", type=int, default=100000, help="number of iterations run for benchmarking")
+    parser.add_argument("--benchmark-dir", type=str, default="./benchmark_files/",
+                        help="directory where benchmark data is saved")
+    parser.add_argument("--plots-dir", type=str, default="./learning_curves/",
+                        help="directory where plot data is saved")
+    return parser.parse_args()
