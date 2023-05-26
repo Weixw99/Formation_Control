e6123711c46c15d2182f2c94146a1f9fd411ba8c
server <xiangwei>
2023-05-26 14:57:58 +0800

    finally

--- config.py
+++ config.py
@@ -68,7 +68,7 @@ class MyWandb:
         self.project_name = 'maddpg_formation'
         self.wandb_user_name = 'weixw99'
         self.pa_name = socket.gethostname()
-        self.exp_name = "side-by-side"+"_train_num: " + str(parameters.max_train_num)
+        self.exp_name = "parallel_apf"+"_train_num: " + str(parameters.max_train_num)
         self.group_name = parameters.scenario
         self.wandb_dir = str(parameters.save_dir)
 
